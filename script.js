498f2a664d7118c97bc25f359261ce6d0610cb2e
Harish Kumar <harishkumarss05@gmail.com>
2021-02-09 13:43:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function disp()
+{
+ var a = document.getElementById("value").value;
+ document.getElementById("output").innerHTML = a;
+}
+
+function clearing()
+{
+ document.getElementById("output").innerHTML = " ";
+}
